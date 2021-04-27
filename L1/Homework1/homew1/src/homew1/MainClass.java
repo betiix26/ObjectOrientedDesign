a3c87f69fe3bdd35ab5e6072b1458f31cfe51e04
betiix26 <81751603+betiix26@users.noreply.github.com>
2021-04-27 20:10:57 +0300

    Add files via upload

---
+++ L1/Homework1/homew1/src/homew1/MainClass.java
@@ -0,0 +1,72 @@
+package homew1;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+        Scanner scan = null;
+        FileWriter output = null;
+        String inputText;
+        try {
+
+            scan = new Scanner(System.in);
+            output = new FileWriter("cuvinte.txt");
+
+            while (true) 
+            {
+            	System.out.println("Introduceti textul : ");
+                inputText = scan.nextLine();
+                if(inputText.equalsIgnoreCase("end"))
+                {
+                	System.out.println("S-a citit textul " + inputText + " si nu se mai pot citi alte cuvinte");
+                }
+                else
+                {
+                System.out.println("Am citit textul: " + inputText);
+                }
+     
+                if (inputText.equalsIgnoreCase("end")) 
+                {
+                    break;
+                }
+
+                output.write(inputText);
+                output.write("\n");
+
+            }
+
+        } catch (IOException e) 
+        	{
+
+          
+            System.out.println("Eroare la citire/afisare");
+
+        	}
+        finally 
+        {
+            if (scan != null) 
+            {
+                scan.close();
+            }
+
+            try {
+
+                if (output != null)
+                {
+                    output.close();
+                }
+
+            } catch (IOException e) 
+            {
+                System.out.println("Fisierul nu s-a inchis!");
+
+            }
+
+        }
+
+	}
+
+}
